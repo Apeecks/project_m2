d89e61a07d8e4d86721d339e28fbcfb5a39e9532
Apeecks <Apeecks@proton.me>
2025-01-30 14:36:26 +0800

    git init

---
+++ src/masks.py
@@ -0,0 +1,15 @@
+def get_mask_card_number(cart_namber: str) -> str:
+    """Маскировка номера банковской карты"""
+    mask_cart_number = []
+    x = cart_namber.replace(cart_namber[6:12], "******")
+    mask_cart_number.append(x[0:4])
+    mask_cart_number.append(x[4:8])
+    mask_cart_number.append(x[8:12])
+    mask_cart_number.append(x[12:16])
+    return " ".join(mask_cart_number)
+
+
+def get_mask_account(account: str) -> str:
+    """Маскировка номера банковского счета"""
+    # mask_account = account.replace(account[:12], "**")
+    return account.replace(account[:12], "**")
