67f5abd22acc8644dc85b32ced165452a8641245
Apeecks <apeecks@Ubuntu.dlink>
2025-02-06 21:24:44 +0300

    fixes masks

--- src/masks.py
+++ src/masks.py
@@ -1,15 +1,13 @@
-def get_mask_card_number(cart_namber: str) -> str:
+def get_mask_card_number(x: str) -> str:
     """Маскировка номера банковской карты"""
-    mask_cart_number = []
-    x = cart_namber.replace(cart_namber[6:12], "******")
-    mask_cart_number.append(x[0:4])
-    mask_cart_number.append(x[4:8])
-    mask_cart_number.append(x[8:12])
-    mask_cart_number.append(x[12:16])
-    return " ".join(mask_cart_number)
+    return x[0:4] + " " + x[4:6] + "**" + " " + "****" + " " + x[12:16]
 
 
 def get_mask_account(account: str) -> str:
     """Маскировка номера банковского счета"""
-    # mask_account = account.replace(account[:12], "**")
     return account.replace(account[:16], "**")
+
+
+# x = "1234123412341234"
+x = "1111111111111111"
+print(get_mask_card_number(x))
